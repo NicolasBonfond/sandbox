dcce1613c8149ff82f140eb4370090d27e2e806e
Nicolas Bonfond <nicolas.bonfond@gaming1.com>
2025-12-24 13:58:08 +0100

    first config

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController {
+    #[Route (path: '/', name: "home")]
+    function index (Request $request): Response {
+        dd($request);
+        return new Response ('Bienvennue ' . $request->query->get('name', 'Invité') . ' sur notre site web !');
+    }
+}
---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+final class RecipeController extends AbstractController
+{
+
+    #[Route('/recette', name: 'recipe.index')]
+    public function index(Request $request): Response
+    {
+        return new Response('Recettes');
+    }
+    
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9\-]+', 'id' => '\d+'])]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+        return $this->json([
+            'slug' => $slug,
+            'id' => $id,
+        ]);
+        return new Response('Recette ' . $slug);
+    }
+}
+
