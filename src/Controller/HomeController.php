26c6f28d339b28966c02e6a8bf54d589f511af56
Nicolas Bonfond <nicolas.bonfond@outlook.com>
2026-01-05 22:54:09 +0100

    controller+twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,9 +8,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController {
+
     #[Route (path: '/', name: "home")]
     function index (Request $request): Response {
-        dd($request);
+        // dd($request);
         return new Response ('Bienvennue ' . $request->query->get('name', 'Invité') . ' sur notre site web !');
     }
+    
 }
